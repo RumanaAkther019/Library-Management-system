da683ec7692403b629e4548e52c82086a3206fac
RumanaAkther019 <154257829+RumanaAkther019@users.noreply.github.com>
2024-01-29 22:38:39 +0600

    make some changes

--- pro-libary managment/main.c
+++ pro-libary managment/main.c
@@ -8,88 +8,77 @@ char book_name[200];
 char title[100];
 char authors_name [50];
 int quantity;
-struct Book *next;
+struct library *next;
 
 };
- Book*new_book(char authors_name, char title,int quantity )
+struct library*head;
+ struct library *create_book(char title[], char authors_name[], int quantity)
+
  {
-     AddBook *new_book = (Book *) malloc(sizeof(Book));
+    struct library *new_book = (struct library *)malloc(sizeof(struct library));
     new_book->quantity = quantity;
     new_book->next = NULL;
     return new_book;
  }
- void add_book(Book *head, char title, char authors_name, int quantity)
- {
-    Book *new_book = create_book(title, author, quantity);
+ void add_book(struct library **head, char title[], char authors_name[], int quantity)
+{
+    struct library *new_book = create_book(title, authors_name, quantity);
     new_book->next = *head;
-    head = new_book;
+    *head = new_book;
 }
- void display_books(Book *head) {
+ void display_books(struct library *head,char title[],char authors_name[]) {
     printf("List of Books:\n");
-    Book *current = head;
-    while (current != NULL) {
-        printf(" %s title: \n" , current->tille);
-        printf("%s authors_name:\n ",current->authors_name);
-        printf("%d quantity:\n",current->quantity);
+    struct library*current = head;
+    while (current != NULL)
+    {
+        printf("Title: %s\n", current->book_name);
+        printf("Author: %s\n", current->authors_name);
+        printf("Quantity: %d\n", current->quantity);
         current = current->next;
     }
 }
 
+int main( ) {
+   struct library *head = NULL;
+    int j;
 
-int main(Book *head) {
-    int j=0;
+    while (1)
+    {
+        printf("-------Library Management System--------\n");
+        printf("1. Add Book details\n2. Display the list of books\n3. Display the total no. of books\n4. Exit\n");
+        printf("Enter the number of your choice: ");
+        scanf("%d", &j);
 
-    while(6)
-{
-    printf ("-------Libary Managment System--------\n");
-     printf(" 1. Add Book details\n 2. Display the list of books \n 3. Display the total no. of books\n 4. Exit\n  ");
-     printf(" Enter the number of your choice:  ");
-    scanf(" %d", &j);
-    switch(j)
+        switch (j)
         {
-             case 1:
-            printf (" Add book details\n : ");
-            printf(" enter the book name\n : ");
-            scanf("%s",lib[i].book_name);
-            printf ("Enter author name \n: ");
-           scanf ("%s",lib[i].authors_name);
+        case 1:
+            printf("Add book details:\n");
+            char title[200], authors_name[50];
+            int quantity;
+            printf("Enter the book name: ");
+            scanf("%s", title);
+            printf("Enter author name: ");
+            scanf("%s", authors_name);
+            printf("Enter quantity: ");
+            scanf("%d", &quantity);
+            add_book(&head, title, authors_name, quantity);
+            break;
 
+        case 2:
+            display_books(head,title,authors_name);
+            break;
 
+        case 3:
+             printf("%d Total number of books in the library are \n:");
+            break;
 
+        case 4:
+            exit(0);
 
-                case 2:
-
-                if (count==0)
-                {
-                    printf(" There are no books stored\n ");
-                }
-                else
-                {
-
-                    printf(" You can view the list of books \n");
-                    printf(" The list of books are\n: ");
-                    for(i=0; i < count; i++)
-                    {
-                        printf("%s The name of the book is\n: ", lib[i].book_name);
-                        printf("%s The name of the author is\n:  ", lib[i].authors_name);
-
-                    }
-                      }
-                break;
-
-            case 3:
-
-                 printf("%d Total number of books in the library are \n: ", count);
-                break;
-
-            case 4:
-                exit(0);
-                 default:
-
-                printf(" Invalid number entered\n");
+        default:
+            printf("Invalid number entered\n");
         }
-
     }
 
-
+    return 0;
 }
